c49125a37bef8d641fff02cf2a114a0f9dabe19b
Mohammad <mohammad.kasirey@gmail.com>
2021-03-03 22:38:38 +0330

    add User migration - controller

--- admin-laravel/routes/api.php
+++ admin-laravel/routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('user'  , "UserController");
+
+
+//Route::get    (  'user'        , [UserController::class  , "index"  ]);
+//Route::get    (  'user/{id}'   , [UserController::class  , "show"   ]);
+//Route::post   (  'user'        , [UserController::class  , "store"  ]);
+//Route::patch  (  'user/{id}'   , [UserController::class  , "update" ]);
+//Route::delete (  'user/{id}'   , [UserController::class  , "destroy"]);
